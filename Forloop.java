f83cf81013a85cb5a10f256f52e7f4eee948850d
dharmapuriyudhveer <dharmapuriyudhveer@gmail.com>
2024-06-27 12:04:44 +0530

    Yudhveer

---
+++ Forloop.java
@@ -0,0 +1,14 @@
+public class Forloop 
+{
+    public static void m1()
+    {
+        for(int i=1;i<20;i++)
+        {
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) 
+    {
+        m1();
+    }
+}
