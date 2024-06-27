7ffa55e17fbf70f3ce724e075c24484ea05b7aa1
Ro-045 <pratapnaidu016@gmail.com>
2024-06-27 22:58:30 +0530

    pratap

---
+++ func.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class func {
+   public  static void fumcfor()
+   {
+    int m;
+    Scanner sc= new Scanner(System.in);
+    System.out.println("enter number:");
+    m=sc.nextInt();
+
+    for(int i=0;i<=m;i++)
+    {
+        System.out.println("rohit century:"+i);
+    }
+}
+     public static void main(String[] args) {
+
+        fumcfor();
+     }
+}
