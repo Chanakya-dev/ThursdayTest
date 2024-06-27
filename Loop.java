cb70cec6986651b67f07a0739dc16659d2712ee6
KOushikguptha <koushikguptha2003@gmail.com>
2024-06-27 12:13:32 +0530

    Koushik

---
+++ Loop.java
@@ -0,0 +1,15 @@
+package ThursdayTest;
+
+public class Loop {
+    public static void main(String args[]) {
+        int numbers[] = {2,3,4,5};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+
+    }
+
+    
+
